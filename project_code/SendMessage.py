d8cacac7bbb7f973ea514e126c2a443684e35e80
Nelai123 <115480885+Nelai123@users.noreply.github.com>
2025-05-27 02:29:41 +0300

    Update SendMessage.py

--- project_code/SendMessage.py
+++ project_code/SendMessage.py
@@ -1,4 +1,5 @@
 import DBManager
+
 class SendMessage:
     def __init__(self, message, sender, receiver):
         self.message = message
@@ -8,14 +9,11 @@ class SendMessage:
     def sendMsg(self):
         db = DBManager.DBManager(database="petato_db")
         db.connect()
-        try:
-            query = "INSERT INTO messages (msg_sender, msg_receiver, msg_text, msg_date)VALUES (%s, %s, %s, NOW())"
-            params = (self.sender, self.receiver, self.message)
-            db.execute_query(query, params)
-            db.connection.commit()
-        except Exception as e:
-            print("Error sending message:", e)
-            
+        db.execute_query(
+            "INSERT INTO messages (msg_sender, msg_receiver, msg_text) VALUES (%s, %s, %s)", 
+            (self.sender, self.receiver, self.message)
+        )
+        db.connection.commit()
+
             
         db.close()
-        
